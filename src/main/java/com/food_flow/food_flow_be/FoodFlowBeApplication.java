a5519cb24a505744004f91639fa6efbfb2be539f
vilarj <lamaravillasan@outlook.com>
2025-03-04 17:02:23 -0500

    Init

---
+++ src/main/java/com/food_flow/food_flow_be/FoodFlowBeApplication.java
@@ -0,0 +1,13 @@
+package com.food_flow.food_flow_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFlowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFlowBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food_flow/food_flow_be/FoodFlowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_flow.food_flow_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFlowBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
